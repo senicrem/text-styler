6a6bc8c42f18c43dbff81ecad4d2f5257dc25cc4
jj22511-dev <jj22511@gmail.com>
2025-01-22 15:53:05 +0800

    vue router setup scaffolding

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Landing from './pages/Landing.vue'
+import Page1 from './pages/Page1.vue'
 
-createApp(App).mount('#app')
+import {
+    createMemoryHistory,
+    createWebHashHistory,
+    createWebHistory,
+    createRouter 
+
+} from 'vue-router'
+
+const routerConfig = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: Landing },
+        { path: '/page1', component: Page1 },
+    ],
+})
+
+createApp(App)
+    .use(routerConfig)
+    .mount('#app')
---
+++ src/pages/Landing.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+
+<template>
+    <div>Landing Page</div>
+    <RouterLink to="/page1">Go to Page1</RouterLink>
+</template>
